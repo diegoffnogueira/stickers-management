0359c0b6ad2a215bcaba4ffcda9c3cd1e1a0ec9c
Diego Nogueira <diegof.fnogueira@gmail.com>
2019-09-16 23:05:59 -0300

    Projeto de figurinhas em ruby... criando Stickers

---
+++ app/controllers/api/v1/stickers_controller.rb
@@ -0,0 +1,44 @@
+module API
+  module V1
+    class StickersController < ApplicationController
+      before_action :load_collection, :load_stickers, only: %i[index]
+      before_action :load_sticker, only: %i[update]
+
+      def index
+        render json: @stickers, status: :ok
+      end
+
+      def update
+        if @sticker.update(sticker_params)
+          render json: @sticker
+        else
+          render json: @sticker.errors, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      # Use callbacks to share common setup or constraints between actions.
+      def load_stickers
+        @stickers = Sticker.by_collection(@collection)
+        head :not_found if @stickers.empty?
+      end
+
+      def load_sticker
+        @sticker = Sticker.by_uuid(params[:id])&.first
+        head :not_found if @sticker.blank?
+      end
+
+      def load_collection
+        @collection = Collection.by_uuid(params[:collection_id])&.first
+        head :not_found if @collection.blank?
+      end
+
+      # Only allow a trusted parameter "white list" through.
+      def sticker_params
+        params.require(:sticker).permit(:quantity)
+      end
+    end
+  end
+end
+
--- app/models/sticker.rb
+++ app/models/sticker.rb
@@ -1,3 +1,15 @@
 class Sticker < ApplicationRecord
   belongs_to :collection
+
+  scope :by_uuid, (->(uuid) { where(uuid: uuid) })
+  scope :by_collection, (->(collection) { where('collection_id = ?', collection.id).order(:number) })
+
+  after_initialize :set_uuid
+
+  validates :number, :quantity, :uuid, presence: true
+  validates :uuid, uniqueness: true
+
+  def set_uuid
+    self.uuid ||= SecureRandom.uuid
+  end
 end
